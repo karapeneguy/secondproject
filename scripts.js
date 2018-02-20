12b45b2fa82a9a99ca7d647c222a5f210e4db211
karapeneguy <helpdesk@helps-imac-234.smc.loyno.edu>
2018-02-20 10:34:13 -0600

    beginning of project 2

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("#overlay-mailinglist").click(function(){
+    $("#overlay").fadeIn();
+        
+    });
+        
+       $("#overlay-close").click(function(){
+           $('#overlay').fadeOut();
+           
+       });
+    
+    
+    
+    
+    
+    
+});
